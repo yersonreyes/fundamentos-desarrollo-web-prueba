29afa9b83d62776696bf4c183fcc3085fc4794f8
Yerson Reyes <yerson.programador@gmail.com>
2021-12-26 20:51:50 -0300

    Arreglos en la identacion del codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
-  $( "#button" ).click(function() {
-    alert( "Mensaje enviado" );
-  });
+$("#button").click(function () {
+  alert("Mensaje enviado");
+});
