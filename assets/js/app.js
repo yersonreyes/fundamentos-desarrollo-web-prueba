9866659697063e8ffd1d1e2a4c564a426d521a6f
Yerson Reyes <yerson.programador@gmail.com>
2021-12-30 21:34:37 -0300

    agregando funcionalidades de js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,26 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
 
-$("#button").click(function () {
-  alert("Mensaje enviado");
+$("#contact-form").submit(function (event) {
+  event.preventDefault();
+  console.log("hola mundo");
+
+  var nombre = $("#inputNombre").val();
+  var asunto = $("#inputAsunto").val();
+  var mensaje = $("#inputMensaje").val();
+
+  console.log("estos son los datos capturados del formulario");
+  console.log({ nombre, asunto, mensaje });
+
+  setTimeout(function () {
+    $("#inputNombre").val("");
+    $("#inputAsunto").val("");
+    $("#inputMensaje").val("");
+    alert("mensaje enviado con exito, pronto alguien te contactara");
+  }, 2000);
+});
+
+$(".title").click(function (event) {
+  var titleElement = event.target;
+  $(titleElement).toggleClass("text-warning");
 });
